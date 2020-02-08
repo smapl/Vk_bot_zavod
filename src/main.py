14bf0b8c39dd2bdcc9c7e8a7ea5984de6a95aca4
smapl <a.mustafin00@mail.ru>
2020-02-08 20:03:08 +0300

    change token & add in messages.send peer_id

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType, VkBotMessageEvent
+import random
 
-api_token = "90b54d3d736030ed7444d4b905adaecff7b896653ff71c4dd58e6d0d0bc85f37dd0e20a07546fd1a5a693"
+api_token = "0688315e5f32dbf06067e661df17c802b21388116366eeccb7fa8cf4f5a53680dc5ee2cd016d74430c007"
 vk_session = vk_api.VkApi(token=api_token)
-longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
+longpoll = VkBotLongPoll(vk=vk_session, group_id="174632651")
 
 for event in longpoll.listen():
-    print(event)
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        if event.text == "как дела?" or event.text == "чо как":
+    if event.type.name == "MESSAGE_NEW" and event.from_chat:
+        text_from_chat = event.message.text.split(" ", 1)[1]
+        if text_from_chat == "как дела?" or text_from_chat == "чо как":
             if event.from_chat:
-                vk.messages.send(chat_id=event.chat_id, message="Всё путем, чувак")
-
-            elif event.from_user:
+                chat_id = event.message.conversation_message_id
+                peer_id = event.message.peer_id
                 vk.messages.send(
-                    user_id=event.user_id, message="так то всё путем", random_id=1234
+                    message="так то всё путем",
+                    # chat_id=chat_id,
+                    random_id=random.getrandbits(32),
+                    peer_id=peer_id,
                 )
+
