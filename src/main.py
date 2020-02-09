c640c530d4e77b15ea1ae2fe1160db0b7e68b8ec
smapl <a.mustafin00@mail.ru>
2020-02-09 16:38:11 +0300

    I hid Vk_token in Linux os via export

--- src/main.py
+++ src/main.py
@@ -1,23 +1,26 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType, VkBotMessageEvent
 import random
+import os
 from response import answer, hello
 
-api_token = "0688315e5f32dbf06067e661df17c802b21388116366eeccb7fa8cf4f5a53680dc5ee2cd016d74430c007"
+api_token = os.environ["vk_token"]
 vk_session = vk_api.VkApi(token=api_token)
 vk = vk_session.get_api()
 longpoll = VkBotLongPoll(vk=vk_session, group_id="174632651")
 
 for event in longpoll.listen():
     to_me = event.message.text.split(" ", 1)[0]
+
     if (
         event.type.name == "MESSAGE_NEW"
         and event.from_chat
         and to_me == "[club174632651|@smap_l]"
     ):
         text_from_chat = event.message.text.split(" ", 1)[1].lower()
-        print(text_from_chat)
+
         if text_from_chat == "привет" or text_from_chat == "здравствуй":
+
             if event.from_chat:
                 chat_id = event.message.conversation_message_id
                 peer_id = event.message.peer_id
@@ -26,6 +29,7 @@ for event in longpoll.listen():
                 )
 
         if text_from_chat == "как дела?" or text_from_chat == "чо как":
+
             if event.from_chat:
                 chat_id = event.message.conversation_message_id
                 peer_id = event.message.peer_id
